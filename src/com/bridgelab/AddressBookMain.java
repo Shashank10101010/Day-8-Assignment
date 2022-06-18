01afd447194423d1358c1cf513a5fe1d475f3483
Shashank10101010 <shashankpraveen23@gmail.com>
2022-06-18 10:36:30 +0530

    Welcome to Address Book Program

---
+++ src/com/bridgelab/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bridgelab;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Address Book Main Program");
+    }
+}
